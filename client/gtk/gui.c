97f30d337b3dcdd91e97bd406880c8febe18e0f8
MaZ-Desktop <jquick@maz-pc.local>
2011-06-14 12:27:35 -0400

    update

--- client/gtk/gui.c
+++ client/gtk/gui.c
@@ -1773,8 +1773,6 @@ GtkWidget *gui_build_interface(void)
 
 	GtkUIManager *mgr = gtk_ui_manager_new();
 	GtkOSXApplication *theApp = g_object_new(GTK_TYPE_OSX_APPLICATION, NULL);
-	mergeid = gtk_ui_manager_add_ui_from_string(mgr,  ui_description, -1, &error);
-
 
 	icon_file =
 	    g_build_filename(g_get_user_runtime_dir(), "pixmaps", PIONEERS_ICON_FILE, NULL);
@@ -1871,9 +1869,8 @@ GtkWidget *gui_build_interface(void)
 	g_signal_connect(G_OBJECT(app_window), "delete_event",
 			 G_CALLBACK(quit_cb), NULL);
 	
-	menubar = gtk_ui_manager_get_widget(mgr, "/menubar");
 	  gtk_widget_hide (menubar);
-	    gtk_osxapplication_set_menu_bar(theApp, GTK_MENU_SHELL(menubar));
+	  gtk_osxapplication_set_menu_bar(theApp, GTK_MENU_SHELL(menubar));
 
 	return app_window;
 }
